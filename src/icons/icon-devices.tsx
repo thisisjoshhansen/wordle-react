c75a7f49922ac7ec6c82d8393e4844633530cb4f
Josh Hansen <thisisjoshhansen@gmail.com>
2024-08-19 11:21:13 -0600

    Adding credit for icons

--- src/icons/icon-devices.tsx
+++ src/icons/icon-devices.tsx
@@ -1,3 +1,5 @@
+// https://fonts.google.com/icons?selected=Material+Symbols+Outlined:devices:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=devices&icon.size=24&icon.color=%235f6368
+
 export default function IconDevices() {
   return <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 -960 960 960" width="1em" fill="currentColor">
     <path d="M80-160v-120h80v-440q0-33 23.5-56.5T240-800h600v80H240v440h240v120H80Zm520 0q-17 0-28.5-11.5T560-200v-400q0-17 11.5-28.5T600-640h240q17 0 28.5 11.5T880-600v400q0 17-11.5 28.5T840-160H600Zm40-120h160v-280H640v280Zm0 0h160-160Z"/>
